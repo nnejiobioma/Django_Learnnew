6e5b1120a1efb5bd5dd6a3e58b54414ac869065b
nnejiobioma <nneji.obioma@gmail.com>
2025-09-30 19:38:14 +0100

    Created templates folder. modified url.py folder, adjuested DIR in settings to recognise template directory

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse # this imports https response
-
+# from django.http import HttpResponse # this imports https response
+from django.shortcuts import render # imports render
 
 
 # Created funtion for each page
 def homepage(request):
-    return HttpResponse("Hello Worl, This is your HOME") # Created the home page to request and respond )
+    # return HttpResponse("Hello Worl, This is your HOME") # Created the home page to request and respond )
+    return render(request, 'home.html')
+
 
 def about(request):
-    return HttpResponse("This is about Page and it is about you") #Created about page
+    # return HttpResponse("This is about Page and it is about you") #Created about page
+    return render(request, 'about.html')
